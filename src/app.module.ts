816ac4ee5b6e180ced1965f68f641baea79a8ecf
Korolenko Daria <admem75213@gmail.com>
2024-06-21 14:08:15 +0300

    create new app

---
+++ src/app.module.ts
@@ -0,0 +1,9 @@
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controllers";
+
+@Module({
+    controllers: [AppController],
+    providers: [],
+})
+
+export class AppModule { };
