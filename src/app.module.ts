fa5746d34a17cbd34c38bd296dca71bd8247d90b
Korolenko Daria <admem75213@gmail.com>
2024-06-24 16:19:11 +0300

    create module, schema, controller, service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from "@nestjs/common";
-import { AppController } from "./app.controllers";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
+    imports: [MongooseModule.forRoot('mongodb+srv://Korolenko:l3iguC2LCPOUXNw2@cluster0.w3zl1n3.mongodb.net/db-todos?retryWrites=true&w=majority')],
     controllers: [AppController],
-    providers: [],
+    providers: [AppService],
+
 })
 
 export class AppModule { };
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,2 +1,7 @@
 import { Injectable } from '@nestjs/common';
-
+@Injectable()
+export class AppService {
+    getUsers() {
+        return [{ id: 1, name: "Daria" }]
+    }
+}
