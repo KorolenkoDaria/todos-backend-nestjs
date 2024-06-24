f21b8f3718fb17fc328dad56e54f5ba47196b163
Korolenko Daria <admem75213@gmail.com>
2024-06-24 16:36:39 +0300

    creating a project structure

---
+++ src/todos/todos.module.ts
@@ -0,0 +1,13 @@
+// todo.module.ts
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Todo, TodoSchema } from './todo.schema';
+import { TodoService } from './todo.service';
+import { TodoController } from './todo.controller';
+
+@Module({
+    imports: [MongooseModule.forFeature([{ name: Todo.name, schema: TodoSchema }])],
+    providers: [TodoService],
+    controllers: [TodoController],
+})
+export class TodoModule { }
